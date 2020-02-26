20681127dca6ed34d32649deae810bdf93551e83
Tran Duc Thanh <ducthanh448@gmail.com>
2020-02-26 10:59:55 +0700

    change
    change

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var path = require('path');
-var port = normalizePort(process.env.PORT || '8087');
-var bodyParser = require('body-parser');
-var server = http.createServer(app);
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/webapps-v2')));
-// app.use(express.static(path.join('/var/www/bb-portal/webapps-v2')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/webapps-v2'));
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/webapps-v2/index.html'))
-  // res.sendFile(path.join('/var/www/bb-portal/webapps-v2/index.html'))
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/webapps-v2/index.html'));
 });
 
-app.listen(port, (req, res) => {
-  console.log(`Blackbox webapps-v2 listening on port ${ port }`);
-});
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-  return false;
-}
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
